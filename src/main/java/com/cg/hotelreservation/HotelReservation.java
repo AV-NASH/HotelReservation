fa1ce8918901c3c63f126a1bec14f8ef09edba11
AV-NASH <avinash21213434@gmail.com>
2020-11-13 21:53:58 +0530

    Added welcome message to the git

---
+++ src/main/java/com/cg/hotelreservation/HotelReservation.java
@@ -0,0 +1,4 @@
+package com.cg.hotelreservation;
+
+public class HotelReservation {
+}
---
+++ src/main/java/com/cg/hotelreservation/HotelReservationUserInterface.java
@@ -0,0 +1,7 @@
+package com.cg.hotelreservation;
+
+public class HotelReservationUserInterface {
+    public static void main(String[] args) {
+        System.out.println("welcome to hotel reservation application");
+    }
+}
