aab0198684b64470c9c90200e6ae5ae1aa2dd180
AV-NASH <avinash21213434@gmail.com>
2020-11-14 18:08:49 +0530

    Added methd to find the cheapest hotel given a date range

--- src/main/java/com/cg/hotelreservation/HotelReservation.java
+++ src/main/java/com/cg/hotelreservation/HotelReservation.java
@@ -1,9 +1,13 @@
 package com.cg.hotelreservation;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class HotelReservation {
     private ArrayList<HotelInfo> hotelInfos=new ArrayList<>();
+    private String cheapestHotel;
+    private  int hotelRate;
 
     public ArrayList<HotelInfo> getHotelInfos() {
         return hotelInfos;
@@ -15,5 +19,31 @@ public class HotelReservation {
 
     public void addNewHotel(String hotelName, int rate) {
         hotelInfos.add(new HotelInfo(hotelName,rate));
+    }
+
+    public void findCheapestHotel(LocalDate start, LocalDate end) {
+        int[] no_of_days=new int[1];
+         no_of_days[0]=0;
+        for(LocalDate localDate=start;localDate.isBefore(end)||localDate.isEqual(end);localDate=localDate.plusDays(1)){
+            no_of_days[0]++;
+        }
+        Optional<HotelInfo> cheapesthotelOptionalObj=hotelInfos.stream().reduce((hotelInfo1, hotelInfo2) ->
+        calculateRent(hotelInfo1,no_of_days[0])<calculateRent(hotelInfo2,no_of_days[0])?hotelInfo1:hotelInfo2);
+
+       HotelInfo cheapestHotelObj= cheapesthotelOptionalObj.get();
+       cheapestHotel=cheapestHotelObj.getHotel_name();
+       hotelRate=calculateRent(cheapestHotelObj,no_of_days[0]);
+    }
+
+    private int calculateRent(HotelInfo hotelInfo,int no_of_days) {
+        return hotelInfo.getRate()*no_of_days;
+    }
+
+    public String getCheapestHotel() {
+        return cheapestHotel;
+    }
+
+    public int getHotelRate() {
+        return hotelRate;
     }
 }
--- src/test/java/com/cg/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/cg/hotelreservation/HotelReservationTest.java
@@ -3,7 +3,9 @@ package com.cg.hotelreservation;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.TreeMap;
 
 import static org.junit.Assert.*;
 
@@ -16,5 +18,20 @@ public class HotelReservationTest {
         hotelReservation.addNewHotel("Ridgewood",220);
         ArrayList<HotelInfo> hotelInfos=hotelReservation.getHotelInfos();
         Assert.assertEquals(3,hotelInfos.size());
+    }
+
+    @Test
+    public void givenDateRangeShouldReturnCheapestHotel() {
+        HotelReservation hotelReservation=new HotelReservation();
+        hotelReservation.addNewHotel("Lakewood",110);
+        hotelReservation.addNewHotel("Bridgewood",160);
+        hotelReservation.addNewHotel("Ridgewood",220);
+        hotelReservation.findCheapestHotel(LocalDate.of(2020,11,11),
+                                              LocalDate.of(2020,11,13));
+
+
+       Assert.assertEquals("Lakewood",hotelReservation.getCheapestHotel());
+       Assert.assertEquals(330,hotelReservation.getHotelRate());
+
     }
 }
